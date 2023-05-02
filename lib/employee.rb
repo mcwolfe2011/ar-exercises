a2b6b5db7600a90090db6220fb6dcfcbfa11ba75
Monica Wolfe <mcwolfe2011@gmail.com>
2023-05-02 15:17:33 -0700

    Created association employee to store

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,3 @@
 class Employee < ActiveRecord::Base
-  belongs_to :store
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :hourly_rate, inclusion: { in: 40..200}
-  validates_associated :store
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,3 @@
 class Store < ActiveRecord::Base
-  has_many :employees
-  validates :name, length: { minimum: 3}
-  validates :annual_revenue, numericality: { greater_than: 0 }
+
 end
